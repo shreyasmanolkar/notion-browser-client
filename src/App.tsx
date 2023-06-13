fc0b3b993883b535ecffb1f1228fa0352c05ffba
shreyas <shreyasmanolkar123@gmail.com>
2023-06-13 23:18:46 +0530

    feat(not-found): added not found page, tests and styls

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Register from "./pages/register";
 import LogIn from "./pages/log-in";
+import NotFound from "./pages/not-found";
 import "./styles/_main.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       {/* <h1>Notion-Clone</h1> */}
       <Register />
       <LogIn />
+      <NotFound />
     </div>
   );
 }
---
+++ src/pages/not-found/__tests__/not-found.spec.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable testing-library/no-render-in-setup */
+import { render, screen } from "@testing-library/react";
+import NotFound from "../NotFound";
+
+describe("NotFound", () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  it("renders correctly", () => {
+    const notionLogo = screen.queryByTestId("notion-logo");
+    const notionTextElement = screen.getByText("Notion");
+    const eyesIcon = screen.queryByTestId("eyes-icon");
+    const titleTextElement = screen.getByText("This content does not exist");
+    const backButtonElement = screen.getByRole("button");
+
+    expect(notionLogo).toBeInTheDocument();
+    expect(notionTextElement).toBeInTheDocument();
+    expect(eyesIcon).toBeInTheDocument();
+    expect(titleTextElement).toBeInTheDocument();
+    expect(backButtonElement).toBeInTheDocument();
+  });
+});
