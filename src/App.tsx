adf4a0fa3c0e43dc8805f2e25776e9be4218ca33
shreyas <shreyasmanolkar123@gmail.com>
2023-06-15 17:05:30 +0530

    refactor(*): refactored code as per review comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import Register from "./pages/register";
-import LogIn from "./pages/log-in";
+// import LogIn from "./pages/log-in";
 // import NotFound from "./pages/not-found";
-// import Workspace from "./pages/workspace";
+import Workspace from "./pages/workspace";
 import "./styles/_main.scss";
 
 function App() {
@@ -9,9 +9,9 @@ function App() {
     <div className="App">
       {/* <h1>Notion-Clone</h1> */}
       {/* <Register /> */}
-      <LogIn />
+      {/* <LogIn /> */}
       {/* <NotFound /> */}
-      {/* <Workspace /> */}
+      <Workspace />
     </div>
   );
 }
