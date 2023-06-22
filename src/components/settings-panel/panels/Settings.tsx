79944f45e2af4227e47c67eb802bab4751fca1a1
shreyas <shreyasmanolkar123@gmail.com>
2023-06-23 02:39:12 +0530

    feat(sidebar menu): added workflow to update workspace icon

--- src/components/settings-panel/panels/Settings.tsx
+++ src/components/settings-panel/panels/Settings.tsx
@@ -7,9 +7,9 @@ import { useDispatch } from "react-redux";
 import { setWorkspace } from "../../../slice/workspaceSlice";
 import { request } from "../../../lib/axios";
 import { setUser } from "../../../slice/userSlice";
-import Workspace from "../../../pages/workspace";
+import EmojiSelector from "../../create-workspace-panel/EmojiSelector";
 
-interface Workspace {
+interface WorkspaceDataType {
   workspaceId: string;
   workspaceName: string;
   workspaceIcon: string;
@@ -21,11 +21,18 @@ const Settings = () => {
   const [name, setName] = useState("");
   const { mutate: mutateUpdateWorkspaceName } =
     useWorkspaceData.useUpdateWorkspaceNameData();
+  const { mutate: mutateUpdateWorkspaceIcon } =
+    useWorkspaceData.useUpdateWorkspaceIconData();
   const workspaceInfo = useAppSelector(
     (state) => state.workspace.workspaceInfo
   );
   const userInfo = useAppSelector((state) => state.user.userInfo);
   const dispatch = useDispatch();
+  const [openPicker, setOpenPicker] = useState(false);
+  const [emojiCode, setEmojiCode] = useState<string>("1f30e");
+  const [emoji, setEmoji] = useState<string>(
+    `https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/${workspaceInfo?.icon}.png`
+  );
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setName(e.target.value);
@@ -57,7 +64,7 @@ const Settings = () => {
           dispatch(setUser({ ...user.data }));
 
           const savedState = localStorage.getItem("workspaceListState");
-          const parsedSavedState: Workspace[] = JSON.parse(savedState!);
+          const parsedSavedState: WorkspaceDataType[] = JSON.parse(savedState!);
 
           for (let i = 0; i < parsedSavedState.length; i++) {
             if (parsedSavedState[i].workspaceId === workspaceId) {
@@ -75,76 +82,145 @@ const Settings = () => {
     });
   };
 
+  const handleIconChange = () => {
+    const workspaceId = workspaceInfo?.id;
+
+    const workspaceData = {
+      icon: emojiCode,
+      workspaceId: workspaceId!,
+    };
+
+    mutateUpdateWorkspaceIcon(workspaceData, {
+      onSuccess: async (data) => {
+        if (data) {
+          const updatedIcon = data.icon;
+          const userId = userInfo?.id;
+          const user = await request({ url: `/users/${userId}` });
+
+          const updatedWorkspace = {
+            ...workspaceInfo!,
+            icon: updatedIcon,
+          };
+
+          dispatch(setWorkspace(updatedWorkspace));
+          dispatch(setUser({ ...user.data }));
+
+          const savedState = localStorage.getItem("workspaceListState");
+          const parsedSavedState: WorkspaceDataType[] = JSON.parse(savedState!);
+
+          for (let i = 0; i < parsedSavedState.length; i++) {
+            if (parsedSavedState[i].workspaceId === workspaceId) {
+              parsedSavedState[i].workspaceIcon = updatedIcon;
+              break;
+            }
+          }
+
+          localStorage.setItem(
+            "workspaceListState",
+            JSON.stringify(parsedSavedState)
+          );
+        }
+      },
+    });
+  };
+
   useEffect(() => {
     setName(workspaceInfo?.name || "");
   }, [workspaceInfo?.name]);
 
+  const handleBrokenImage = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.src =
+      "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f30e.png";
+    e.currentTarget.onerror = null;
+  };
+
   return (
-    <div className={`${styles.panel} ${styles[theme]}`}>
-      <div>
-        <div className={`${styles.title}`}>Workspace settings</div>
-        <div className={`${styles.body}`}>
-          <div className={`${styles.container}`}>
-            <div className={`${styles.change_name}`}>
-              <p>Name</p>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  id="name"
-                  value={name}
-                  onChange={handleNameChange}
-                />
-                <button type="submit">Change name</button>
-              </form>
-              <p>
-                You can use your organization or company name. Keep it simple.
-              </p>
+    <>
+      <div className={`${styles.panel} ${styles[theme]}`}>
+        <div>
+          <div className={`${styles.title}`}>Workspace settings</div>
+          <div className={`${styles.body}`}>
+            <div className={`${styles.container}`}>
+              <div className={`${styles.change_name}`}>
+                <p>Name</p>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={handleNameChange}
+                  />
+                  <button type="submit">Change name</button>
+                </form>
+                <p>
+                  You can use your organization or company name. Keep it simple.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className={`${styles.divider}`}></div>
-          <br />
-          <div className={`${styles.value_control}`}>
-            <div className={`${styles.key}`}>
-              <p>Icon</p>
-              {/* Update Icon here */}
-              <p>
-                Upload an image or pick an emoji. It will show up in your
-                sidebar and notifications.
-              </p>
+            <div className={`${styles.divider}`}></div>
+            <br />
+            <div className={`${styles.value_control}`}>
+              <div className={`${styles.key}`}>
+                <p>Icon</p>
+                <div
+                  className={`${styles.emoji_display}`}
+                  onClick={() => setOpenPicker(true)}
+                >
+                  <img
+                    src={emoji}
+                    alt="emoji"
+                    width={50}
+                    onError={handleBrokenImage}
+                  />
+                </div>
+                <p>
+                  Upload an image or pick an emoji. It will show up in your
+                  sidebar and notifications.
+                </p>
+              </div>
+              <div className={`${styles.control} ${styles.valid_button}`}>
+                <button onClick={handleIconChange}>Set Icon</button>
+              </div>
             </div>
-          </div>
-          <br />
-          <div className={`${styles.divider}`}></div>
-          <br />
-          <br />
-        </div>
-      </div>
-      <div>
-        <div className={`${styles.title}`}>Public settings</div>
-        <div className={`${styles.body}`}>
-          <div className={`${styles.value_control}`}>
-            <div className={`${styles.key}`}>
-              <p>Domain</p>
-              <p>
-                Pages shared to web will be under
-                endurable-part-ridge-afb.notion.site. Anyone with an allowed
-                email domain can join this workspace via
-                www.notion.so/endurable-part-ridge-afb.
-              </p>
-            </div>
-          </div>
-          <div className={`${styles.value_control}`}>
-            <div className={`${styles.key}`}>
-              <p>Danger zone</p>
-              <p>Delete Entire Workspace</p>
-            </div>
-            <div className={`${styles.danger_button}`}>
-              <button>Delete</button>
-            </div>
+            <br />
+            <div className={`${styles.divider}`}></div>
+            <br />
+            <br />
           </div>
         </div>
+        <div>
+          <div className={`${styles.title}`}>Public settings</div>
+          <div className={`${styles.body}`}>
+            <div className={`${styles.value_control}`}>
+              <div className={`${styles.key}`}>
+                <p>Domain</p>
+                <p>
+                  Pages shared to web will be under
+                  endurable-part-ridge-afb.notion.site. Anyone with an allowed
+                  email domain can join this workspace via
+                  www.notion.so/endurable-part-ridge-afb.
+                </p>
+              </div>
+            </div>
+            <div className={`${styles.value_control}`}>
+              <div className={`${styles.key}`}>
+                <p>Danger zone</p>
+                <p>Delete Entire Workspace</p>
+              </div>
+              <div className={`${styles.danger_button}`}>
+                <button>Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <EmojiSelector
+        openPicker={openPicker}
+        closePicker={() => setOpenPicker(false)}
+        setEmoji={setEmoji}
+        setEmojiCode={setEmojiCode}
+      />
+    </>
   );
 };
 
--- src/services/useWorkspaceData.ts
+++ src/services/useWorkspaceData.ts
@@ -7,6 +7,11 @@ type updateWorkspaceNameType = {
   workspaceId: string;
 };
 
+type updateWorkspaceIconType = {
+  icon: string;
+  workspaceId: string;
+};
+
 export class useWorkspaceData {
   static updateWorkspaceName = async (data: updateWorkspaceNameType) => {
     const response = await request({
@@ -18,8 +23,27 @@ export class useWorkspaceData {
     return response.data;
   };
 
+  static updateWorkspaceIcon = async (data: updateWorkspaceIconType) => {
+    const response = await request({
+      url: `/workspaces/${data.workspaceId}`,
+      method: "patch",
+      data,
+    });
+
+    return response.data;
+  };
+
   static useUpdateWorkspaceNameData = () => {
     return useMutation(useWorkspaceData.updateWorkspaceName, {
+      onSuccess: (data) => {
+        return data;
+      },
+      onError: (error: AxiosError) => {},
+    });
+  };
+
+  static useUpdateWorkspaceIconData = () => {
+    return useMutation(useWorkspaceData.updateWorkspaceIcon, {
       onSuccess: (data) => {
         return data;
       },
